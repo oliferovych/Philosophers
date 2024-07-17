8e0114494332160bf0b07fae4f070b8002f97470
oliferovych <dimasikolif@gmail.com>
2024-07-17 23:16:44 +0200

    utils + parsing

--- src/parse.c
+++ src/parse.c
@@ -6,26 +6,84 @@
 /*   By: dolifero <dolifero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 20:01:23 by dolifero          #+#    #+#             */
-/*   Updated: 2024/07/16 20:18:30 by dolifero         ###   ########.fr       */
+/*   Updated: 2024/07/17 23:08:24 by dolifero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../philo.h"
 
-int	parse_args(char **argv, int argc, t_philo **philo)
+int	check_args(char **argv, int argc)
 {
-	int	philo_amount;
+	if (argc < 5 || argc > 6)
+		return (printf("Insufficient number of arguments.\n"), -1);
+	return (1);
+}
+
+int	mutex_init(t_data *data)
+{
 	int	i;
 
 	i = 0;
-	philo_amount = atoi(argv[1]);
-	philo = malloc(sizeof(t_philo) * philo_amount + 1);
-	while (i < philo_amount)
+	while (i < data->philo_amount)
 	{
-		philo[i]->num = i + 1;
-		if (argv[5] != NULL)
-			philo[i]->meal_amount = atoi(argv[5]);
-
+		if (pthread_mutex_init(&data->forks[i], NULL) != 0)
+		{
+			free(data->forks);
+			free(data);
+			return (-1);
+		}
 	}
+	if (pthread_mutex_init(&data->print_mutex, NULL) != 0)
+	{
+		free(data->forks);
+		free(data);
+		return (-1);
+	}
+	return (1);
+}
 
+int	philos_init(t_data *data)
+{
+	int	i;
+
+	i = 0;
+	while (i < data->philo_amount)
+	{
+		data->philos[i].num = i;
+		data->philos[i].meals_eaten = 0;
+		data->philos[i].last_meal_t = data->start_time;
+		if (pthread_create(&data->philos[i].thread, NULL,
+				routine, &data->philos[i]) != 0)
+		{
+			free(data->forks);
+			free(data->philos);
+			free(data);
+			return (-1);
+		}
+	}
+	return (1);
+}
+
+int	parse_args(char **argv, int argc, t_data *data)
+{
+	data = malloc(sizeof(t_data));
+	data->philo_amount = ft_atoi(argv[1]);
+	data->die_time = ft_atoi(argv[2]);
+	data->eat_time = ft_atoi(argv[3]);
+	data->sleep_time = ft_atoi(argv[4]);
+	if (argc == 6)
+		data->meal_amount = ft_atoi(argv[5]);
+	else
+		data->meal_amount = -1;
+	data->start_time = ft_get_time();
+	data->philos = malloc(sizeof(t_philo) * data->philo_amount);
+	if (!data->philos)
+		return (-1);
+	data->forks = malloc(sizeof(pthread_mutex_t) * data->philo_amount);
+	if (!data->forks)
+		return (-1);
+	if (!mutex_init(data))
+		return (-1);
+	if (!philos_init(data))
+		return (-1);
 }
---
+++ src/utils.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dolifero <dolifero@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/17 19:34:38 by dolifero          #+#    #+#             */
+/*   Updated: 2024/07/17 22:53:46 by dolifero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../philo.h"
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	negativity;
+	int	result;
+
+	negativity = 1;
+	i = 0;
+	result = 0;
+	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t'
+		|| str[i] == '\r' || str[i] == '\v' || str[i] == '\f')
+		i++;
+	if ((str[i] == '-' || str[i] == '+'))
+	{
+		if (str[i] == '-')
+			negativity *= (-1);
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result *= 10;
+		result += str[i] - '0';
+		i++;
+	}
+	return (result * negativity);
+}
+
+void	ft_usleep(useconds_t usec)
+{
+	long long	start;
+
+	start = ft_get_time();
+	while ((ft_get_time() - start) < usec)
+	{
+		usleep(100);
+	}
+}
+
+long long	ft_get_time(void)
+{
+	struct timeval	time;
+
+	gettimeofday(&time, NULL);
+	return ((time.tv_sec * 1000) + (time.tv_usec / 1000));
+}
+
+void	print_action(t_data *data, t_philo *philo, const char *str)
+{
+	pthread_mutex_lock(&data->print_mutex);
+	printf("%lld %d %s\n", ft_get_time() - data->start_time,
+		(philo->num + 1), str);
+	pthread_mutex_unlock(&data->print_mutex);
+}
